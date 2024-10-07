cc0730c63345eb463e46cfc9109cd2f4e1ea7097
Phani-TrainingPrograms <131134278+Phani-TrainingPrograms@users.noreply.github.com>
2024-10-07 16:00:44 +0530

    Add files via upload

---
+++ Ex01BasicOutput.py
@@ -0,0 +1,19 @@
+# First program in Python.
+# print("Hello World")
+# print("My Name is PHANI RAJ")
+# print("I am from Bangalore")
+# print("My profession is Training!!!")
+# print("I also do consultancy in .NET and Javascript")
+
+print("My name is Phaniraj", end=" ")
+# We wish to end the line by a space, not by moving to the next line
+print("I am from Bangalore", end=",")
+print("My Profession is training!!!")
+
+# print is a function in Python to print text on the output window. Here the output will be the
+# console window
+# Commenting: Ctrl + K, C
+# UnCommenting: Ctrl + K, U
+
+# Todo: Try printing UR details on the Console.
+
---
+++ Ex04Typecasting.py
@@ -0,0 +1,13 @@
+# typecasting : Converting the data type of value to another type.
+
+x = 1
+y = 2.5
+z = "3"
+
+x += int(z) # IF U want to add x with z, z being string should be temporarily casted to int.
+print(x)
+x = y
+print(int(y))
+# convert int to flaat:
+y = float(x);
+print(y)
